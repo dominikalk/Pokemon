09b8bab19985fc7bbc3260333ac79245265c3128
Dominik Alkhovik <dominik.alk@hotmail.com>
2019-10-28 16:17:32 +0000

    individual pokemon images show

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -2,29 +2,32 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const Pokemon = props => {
-  const [pokemon, setPokemon] = useState([]);
+  const [pokemon, setPokemon] = useState();
   useEffect(() => {
     axios
       .get(`https://pokeapi.co/api/v2/pokemon/${props.match.params.name}`)
       .then(res => setPokemon(res.data));
   }, []);
-  console.log(pokemon);
+
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-4">
-          <img
-            //src={require("../assets/pokemon.jpg")}
-            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${1}.png`}
-            className="card-img-top"
-            alt={`${pokemon.name} pokemon`}
-          />
+    <div className="nes-container is-rounded mt-3">
+      {pokemon && (
+        <div className="row">
+          <div className="col-4">
+            <img
+              //src={require("../assets/pokemon.jpg")}
+
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
+              className="card-img-top"
+              alt={`${pokemon.name} pokemon`}
+            />
+          </div>
+          <div className="col-8 my-auto">
+            <p>Height: {pokemon.height}</p>
+            <p>Weight: {pokemon.weight}</p>
+          </div>
         </div>
-        <div className="col-8 my-auto">
-          <p>Height: {pokemon.height}</p>
-          <p>Weight: {pokemon.weight}</p>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
